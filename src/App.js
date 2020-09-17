0d7aabd86078059b47728dc55711ebf49bb8ff20
清川　裕太郎 <yuu7353@gmail.com>
2020-09-17 21:00:24 +0900

    show stargazers

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React, { Component } from 'react';
-import { ApolloProvider } from 'react-apollo';
-import { Query } from 'react-apollo';
-import client from './client';
-import { SEARCH_REPOSITORIES } from './graphql';
+import React, { Component } from 'react'
+import { ApolloProvider } from 'react-apollo'
+import { Query } from 'react-apollo'
+import client from './client'
+import { SEARCH_REPOSITORIES } from './graphql'
 
-const PER_PAGE = 5;
+const StarButton = props => {
+  const totalCount = props.node.stargazers.totalCount 
+  return <button>{totalCount === 1 ? "1 star" : `${totalCount} stars`}</button>
+}
+
+const PER_PAGE = 5
 const DEFAULT_STATE = {
   first: PER_PAGE,
   after: null,
   last: null,
   before: null,
-  query: 'フロントエンドエンジニア'
-};
+  query: "フロントエンドエンジニア"
+}
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = DEFAULT_STATE;
+    super(props)
+    this.state = DEFAULT_STATE
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event) {
     this.setState({
       ...DEFAULT_STATE,
       query: event.target.value
-    });
+    })
   }
 
   handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
   }
 
   goPrevious(search) {
@@ -39,7 +44,7 @@ class App extends Component {
       after: null,
       last: PER_PAGE,
       before: search.pageInfo.startCursor
-    });
+    })
   }
 
   goNext(search) {
@@ -48,12 +53,11 @@ class App extends Component {
       after: search.pageInfo.endCursor,
       last: null,
       before: null
-    });
+    })
   }
 
   render() {
-    const { query, first, last, before, after } = this.state;
-    console.log({ query });
+    const { query, first, last, before, after } = this.state
 
     return (
       <ApolloProvider client={client}>
@@ -64,51 +68,62 @@ class App extends Component {
           query={SEARCH_REPOSITORIES}
           variables={{ query, first, last, before, after }}
         >
-          {({ loading, error, data }) => {
-            if (loading) return 'Loading...';
-            if (error) return `Error! ${error.message}`;
+          {
+            ({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
 
-            const search = data.search;
-            const repositoryCount = search.repositoryCount;
-            const repositoryUnit =
-              repositoryCount === 1 ? 'Repository' : 'Repositories';
-            const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`;
-            return (
-              <React.Fragment>
-                <h2>{title}</h2>
-                <ul>
-                  {search.edges.map(edge => {
-                    const node = edge.node;
+              const search = data.search
+              const repositoryCount = search.repositoryCount
+              const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
+              const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
+              return (
+                <React.Fragment>
+                  <h2>{title}</h2>
+                  <ul>
+                    {
+                      search.edges.map(edge => {
+                        const node = edge.node
 
-                    return (
-                      <li key={node.id}>
-                        <a
-                          href={node.url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {node.name}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </ul>
+                        return (
+                          <li key={node.id}>
+                            <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
+                            &nbsp;
+                            <StarButton node={node}/>
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
 
-                {search.pageInfo.hasPreviousPage === true ? (
-                  <button onClick={this.goPrevious.bind(this, search)}>
-                    Previous
-                  </button>
-                ) : null}
-                {search.pageInfo.hasNextPage === true ? (
-                  <button onClick={this.goNext.bind(this, search)}>Next</button>
-                ) : null}
-              </React.Fragment>
-            );
-          }}
+                  {
+                    search.pageInfo.hasPreviousPage === true ?
+                      <button
+                        onClick={this.goPrevious.bind(this, search)}
+                      >
+                        Previous
+                      </button>
+                      :
+                      null
+                  }
+                  {
+                    search.pageInfo.hasNextPage === true ?
+                      <button
+                        onClick={this.goNext.bind(this, search)}
+                      >
+                        Next
+                      </button>
+                      :
+                      null
+                  }
+                </React.Fragment>
+              )
+            }
+          }
         </Query>
       </ApolloProvider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
